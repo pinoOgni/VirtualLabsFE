f9fcd1dd5f141bbf1ebbe61ff8f0f7263ced7bae
alex <alenihilist@gmail.com>
2021-01-24 20:10:31 +0100

    to be finished add course dialog component

---
+++ src/app/modals/add-course-dialog/add-course-dialog.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-add-course-dialog',
+  templateUrl: './add-course-dialog.component.html',
+  styleUrls: ['./add-course-dialog.component.css']
+})
+export class AddCourseDialogComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
