a352ec3727bfbad2ea860ad3c6e7d99228c08a2d
pinoOgni <ognibenegiuseppe8@gmail.com>
2021-01-30 09:16:43 +0100

    new models: assignement, homework, homework-version; refactor tab teacher/students

---
+++ src/app/models/homework.model.ts
@@ -0,0 +1,36 @@
+/**
+ * This enum represents the status of the homework for a student
+ * NULL = The student has not yet read the delivery
+ * READ = The student has read the delivery
+ * SUBMITTED = The student submitted the assignment
+ * REVIEWED = the teacher revised the test
+ * DEFINITELY_REVIEWED = the professor has definitely reviewed the test and should put a grade
+ * SCORED = the professor gave the homework a score
+ */
+export enum HomeworkStatus {
+    NULL = 'NULL', 
+    READ = 'READ',
+    SUBMITTED = 'SUBMITTED',
+    REVIEWED = 'REVIEWED',
+    DEFINITELY_REVIEWED = 'DEFINITELY_REVIEWED',
+    SCORED = 'SCORED'
+}
+
+/**
+ * This class represent a homework of a student
+ * the assignment_id + student_id can give us the homweork of a student
+ * thanks to the status the teacher can filter the homeworks
+ */
+export class Homework {
+    assignment_id: number;
+    student_id: string;
+    status: HomeworkStatus;
+    score: number;
+
+    constructor(assignment_id: number, student_id: string , status: number = 0, score: number = 0) {
+        this.assignment_id = assignment_id;
+        this.student_id = student_id;
+        this.status = HomeworkStatus[HomeworkStatus[status]];
+        this.score = score;
+    }
+}
